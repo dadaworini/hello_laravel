2f279919017771e9d561e160ffd40a34aafe091e
wu <270291816@qq.com>
2025-08-18 19:53:07 +0800

    初始化样式

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+//     resolve: {
+//        alias: {
+//            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+//        }
+//    },
+   server: {
+        // [添加开始]
+        hmr: {
+            host: 'localhost',
+        },
+    }, // [添加结束]
 });
